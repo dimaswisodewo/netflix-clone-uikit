58883ab90f1d9eaa70710a4765631f87d40b2fe1
dimaswisodewo <dimas@octagonstudio.com>
2023-06-01 12:20:07 +0700

    Setup home CollectionTableViewCell

---
+++ Netflix Clone/Views/HeroHeaderUIView.swift
@@ -0,0 +1,33 @@
+//
+//  HeroHeaderUIView.swift
+//  Netflix Clone
+//
+//  Created by Dimas Wisodewo on 01/06/23.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+
+    private let heroImageView: UIImageView = {
+        
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
